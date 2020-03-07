47a7b1d9a91996b2d195ccc608118243d5062c6f
Juan Chávez <im.jhcc@gmail.com>
2020-03-07 18:35:47 -0400

    Added movie rental model.

---
+++ src/main/java/com/juan/movies/model/MovieRental.java
@@ -0,0 +1,23 @@
+package com.juan.movies.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class MovieRental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+    private Date date;
+    private Date toReturnDate;
+    private Date returnedDate;
+    private float price;
+    @Column(name="status",columnDefinition = "varchar(25) default NULL")
+    private String status;
+}
